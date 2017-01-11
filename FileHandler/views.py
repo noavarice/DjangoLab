5e4ac247a1c4325b37bf9823e7e15bfd22743a18
alexrazinkov <alex9razinkov@gmail.com>
2017-01-11 16:03:17 +0300

    Added restriction on file download

--- FileHandler/views.py
+++ FileHandler/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts   import render, get_object_or_404
-from django.http        import HttpResponseRedirect, HttpResponse
+from django.http        import HttpResponseRedirect, HttpResponse, FileResponse
 from forms              import FileHandlerForm
 from models             import FileHandler, Advertisement
 from base62             import encode, decode
 from magic              import from_file
 from Lab5.settings      import MEDIA_ROOT
-from django.utils.encoding import smart_str
 from random             import randint
+from django.utils       import timezone
+from dateutil.parser    import parse
+from django.utils.encoding import smart_str
 import os
 import urllib
 import string, random
@@ -29,6 +31,9 @@ def succeed(request):
 def download(request):
     unique_file_url = request.path.split('/')[-1]
     obj = get_object_or_404(FileHandler, pk = decode(unique_file_url))
+    if not request.session.session_key:
+        request.session.save()
+    request.session['opening_time'] = str(timezone.now())
     return render(request, 'FileHandler/download_layout.html', {
         'url': unique_file_url,
         'file_name': obj.file_to_store.name.split('/')[-1],
@@ -38,10 +43,15 @@ def download(request):
 def temporary_download_page(request):
     url = request.path.split('/')[-1]
     obj = get_object_or_404(FileHandler, pk = decode(url))
-    full_filename = obj.file_to_store.name
-    if os.path.exists(full_filename):
-        with open(full_filename, 'rb') as f:
-            response = HttpResponse(f, content_type = 'application/force-download')
-            response['Content-Disposition'] = 'inline; filename="%s"' % os.path.basename(full_filename)
-            return response    
+    path = obj.file_to_store.name
+    if os.path.exists(path):
+        exposition_time = obj.bound_advert.exposition_time
+        rs = request.session
+        key_name = 'opening_time'
+        if not rs.session_key or key_name not in rs or (timezone.now() - parse(rs[key_name])).total_seconds() < exposition_time:
+            return HttpResponseRedirect('/download/%s' % request.path.split('/')[-1])
+        del request.session[key_name]
+        response = FileResponse(open(path, 'rb'), content_type = 'application/force-download')
+        response['Content-Disposition'] = 'inline; filename="%s"' % os.path.basename(path)
+        return response    
     return HttpResponseNotFound()
